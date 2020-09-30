e26f4dc851db7147e40e6af6e3c0fcaeaaf028e0
Taras Khomyn <taraskhomyn@MacBook-Air-Taras.local>
2020-09-30 21:22:39 +0300

    Updates

---
+++ precache-manifest.309a8793520018dfa80b0bab12fd396a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccd202909208a3890790c4a1b5207014",
+    "url": "/react-router/index.html"
+  },
+  {
+    "revision": "5d45840c439a204b423b",
+    "url": "/react-router/static/css/main.d586415a.chunk.css"
+  },
+  {
+    "revision": "d81824aad69a9dfc3c84",
+    "url": "/react-router/static/js/2.e63f72ed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-router/static/js/2.e63f72ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d45840c439a204b423b",
+    "url": "/react-router/static/js/main.67479346.chunk.js"
+  },
+  {
+    "revision": "29ab7cd581a7767f6c71",
+    "url": "/react-router/static/js/runtime-main.7f93213d.js"
+  },
+  {
+    "revision": "d964e0b3c644193e5591758f01e91f68",
+    "url": "/react-router/static/media/lock.d964e0b3.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-router/static/media/logo.5d5d9eef.svg"
+  }
+]);
